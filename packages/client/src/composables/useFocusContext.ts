9d2a309d1d12af622bb08be1f08caa4e75866e79
Kyler Fullerton <fullertonkyler@gmail.com>
2026-07-18 12:20:54 -0600

    commit: useFocusable and useFocusContext composables

--- packages/client/src/composables/useFocusContext.ts
+++ packages/client/src/composables/useFocusContext.ts
@@ -11,7 +11,7 @@ import { ROOT_FOCUS_KEY } from '@noriginmedia/norigin-spatial-navigation-core'
 
 export const FocusContextKey: InjectionKey<MaybeRefOrGetter<string>> = Symbol('FocusContext')
 
-export function provideFocusContext(focusKey: MaybeRefOrGetter<string>) {
+export function provideFocusContext(focusKey: MaybeRefOrGetter<string>): void {
   provide(FocusContextKey, focusKey)
 }
 
@@ -21,13 +21,3 @@ export function useFocusContext(): ComputedRef<string> {
   const context = inject(FocusContextKey, ROOT_FOCUS_KEY)
   return computed(() => toValue(context))
 }
-
-// React
-// import { useContext, createContext } from 'react'
-// import { ROOT_FOCUS_KEY } from '@noriginmedia/norigin-spatial-navigation-core'
-
-// export const FocusContext = createContext(ROOT_FOCUS_KEY)
-// FocusContext.displayName = 'FocusContext'
-
-// /** @internal */
-// export const useFocusContext = () => useContext(FocusContext)
